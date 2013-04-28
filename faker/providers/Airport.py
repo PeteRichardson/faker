d301cb97cc56a16a9c34df7dd3934e068a8e2636
Pete Richardson <pete@peterichardson.com>
2013-04-27 21:39:43 -0700

    Use airport data from openflights.org/data

--- faker/providers/Airport.py
+++ faker/providers/Airport.py
@@ -2,20 +2,46 @@ from . import BaseProvider
 
 
 class Airport:
-    def __init__(self, code, name, location):
-        self.code = code
+    """Airport object definition derived from data available at
+       http://www.openflights.org/data
+
+        Airport ID  Unique OpenFlights identifier for this airport.
+        Name        Name of airport. May or may not contain the City name.
+        City        Main city served by airport. May be spelled differently from Name.
+        Country     Country or territory where airport is located.
+        IATA/FAA    3-letter FAA code, for airports located in Country "United States of America".
+                    3-letter IATA code, for all other airports.
+                    Blank if not assigned.
+        ICAO        4-letter ICAO code.
+                    Blank if not assigned.
+        Latitude    Decimal degrees, usually to six significant digits. Negative is South, positive is North.
+        Longitude   Decimal degrees, usually to six significant digits. Negative is West, positive is East.
+        Altitude    In feet.
+        Timezone    Hours offset from UTC. Fractional hours are expressed as decimals, eg. India is 5.5.
+        DST         Daylight savings time. One of E (Europe), A (US/Canada), S (South America), O (Australia), Z (New Zealand), N (None) or U (Unknown).
+       """
+    def __init__(self, id, name, city, country, iata, icao, latitude, longitude, altitude, timezone, DST):
+        self.id = int(id)
         self.name = name
-        self.location = location
+        self.city = city
+        self.country = country
+        self.iata = iata
+        self.icao = '' if icao == "\\N" else icao
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+        self.altitude = int(altitude)
+        self.timezone = timezone
+        self.DST = DST
 
     def __str__(self):
-        return "{0} ({1})".format(self.name, self.code)
+        if not self.icao:
+            return self.name
+        else:
+            return "{0} ({1})".format(self.name, self.icao)
 
 
 class Provider(BaseProvider):
-
-    airports = [{'name': 'San Francisco International Airport', 'code': 'SFO', 'location': 'San Francisco, CA'},
-                {'name': 'Seattle Tacoma International Airport', 'code': 'SEA', 'location': 'Seattle, WA'},
-                {'name': 'McCarran International Airport', 'code': 'LAS', 'location': 'Las Vegas, NV'}]
+    airports = [{"id": "3", "name": "Mount Hagen", "city": "Mount Hagen", "country": "Papua New Guinea", "iata": "HGU", "icao": "AYMH", "latitude": "-5.826789", "longitude": "144.295861", "altitude": "5388", "timezone": "10", "DST": "U"}, ]
 
     @classmethod
     def airport(cls):
@@ -31,11 +57,6 @@ class Provider(BaseProvider):
         list above. Most faker functions return a string, but I didn't want to
         lose all the rich airport info (e.g. code, location) or encode it in a
         string then have to parse it out again.
-
-        I think this model would be useful for other faker providers.
-        For example  faker.family() could return a list of people objects each
-        of which has a name, a gender, a role (Father, Mother, Son, Aunt), etc.
-        It remains to be seen if this is stretching the faker interface too far.
         """
         return Airport(**cls.randomElement(cls.airports))
 
@@ -44,7 +65,7 @@ class Provider(BaseProvider):
         """
         :example 'SFO'
         """
-        return cls.airportObject().code
+        return cls.airportObject().iata
 
     @classmethod
     def airportName(cls):
@@ -56,6 +77,7 @@ class Provider(BaseProvider):
     @classmethod
     def airportLocation(cls):
         """
-        :example 'Las Vegas, NV'
+        :example 'San Francisco, United States'
         """
-        return cls.airportObject().location
+        airport = cls.airportObject()
+        return "{0}, {1}".format(airport.city, airport.country)
--- faker/providers/en_US/Airport.py
+++ faker/providers/en_US/Airport.py
@@ -1,16 +1,14 @@
 from ..Airport import Provider as AirportProvider
-from json import load
 import os
+import csv
 
 
 class Provider(AirportProvider):
     def __init__(self, generator):
-        """ Load the airports list with some airports with English names """
+        """ Load the airplines list from airlines.dat """
+        self.__class__.airports[:] = []    # clear the list
+        fieldnames = ["id", "name", "city", "country", "iata", "icao", "latitude", "longitude", "altitude", "timezone", "DST"]
         dir = os.path.dirname(__file__)
-        with open(os.path.join(dir, 'airports.json')) as airportData:
-            self.__class__.airports = load(airportData, object_hook=self.ascii_encode_dict)
+        with open(os.path.join(dir, 'airports.dat')) as airportData:
+            self.__class__.airports = [row for row in csv.DictReader(airportData, delimiter=",", fieldnames=fieldnames)]
 
-    def ascii_encode_dict(self, data):
-        '''http://stackoverflow.com/questions/9590382/forcing-python-json-module-to-work-with-ascii'''
-        ascii_encode = lambda x: x.encode('ascii', 'ignore')
-        return dict(map(ascii_encode, pair) for pair in data.items())
