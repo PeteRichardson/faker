372d767109cc1401dbfa73c338f5d8dd0b11b280
Pete Richardson <pete@peterichardson.com>
2013-04-27 21:02:02 -0700

    Use Airline data from openflights.org.  Provide new airlineObject() function.

--- faker/providers/Airline.py
+++ faker/providers/Airline.py
@@ -1,15 +1,48 @@
 from . import BaseProvider
 
+class Airline:
+    """Airline object definition derived from data available at
+       http://www.openflights.org/data
+
+       id is the openflights.org id for the airline.
+       \N in a field means no value.
+       iata is the 2 char abbreviation (e.g.  United Airlines = UL)
+       icao is the 3 char abbreviation (e.g.  United Airlines = UAL)
+       active is a boolean indicating the airline is still running. This field
+                  is not particularly trustworthy.
+       """
+    def __init__(self, id, name, alias, iata, icao, callsign, country, active):
+        self.id = id
+        self.name = name
+        #self.alias = '' if alias == "\\N" else alias
+        if alias == "\\N":
+            self.alias = ""
+        else:
+            self.alias = alias
+        self.iata = iata
+        self.icao = icao
+        self.callsign = callsign
+        self.country = country
+        self.active = active == "Y"
+
+    def __str__(self):
+        return "{0} ({1})".format(self.name, self.icao)
+
 
 class Provider(BaseProvider):
 
-    formats = ['{{airline}}', ]
+    airlines = [{"id": 2935, "name": "Island Air Charters", "alias": "\N", "iata": "", "icao": "ILF", "callsign": "ISLAND FLIGHT", "country": "United States", "active": "N"},
+                {"id": 8568, "name": "Trans Maldivian Airways", "alias": "", "iata": "M8", "icao": "TMW", "callsign": "", "country": "Maldives", "active": "N"},
+                {"id": 17618, "name": "AirOne Polska", "alias": "AirOne Polska", "iata": "U1", "icao": "001", "callsign": "AOC", "country": "Poland", "active": "N"}, ]
 
-    airlines = ['Qantas', 'Aeroflot', ]
+    @classmethod
+    def airline(cls):
+        """
+        returns a string constructed from the airline name and icao abbreviation
+        :example 'Worldwide Jet Charter (WWI)'
+        """
+        return str(cls.airlineObject())
 
-    def airline(self):
-        """
-        :example 'Nevada Jets'
-        """
-        format = self.randomElement(self.formats)
-        return self.generator.parse(format)
+    @classmethod
+    def airlineObject(cls):
+        return Airline(**cls.randomElement(cls.airlines))
--- faker/providers/en_US/Airline.py
+++ faker/providers/en_US/Airline.py
@@ -1,52 +1,14 @@
 
+import csv
+import os
+from ..Airline import Provider as AirlineProvider
 
-from ..Airline import  Provider as AirlineProvider
 
 class Provider(AirlineProvider):
-
-    formats = (
-        '{{airline}}',
-        '{{lastName}} {{airlineSuffix}}',
-        '{{state}} {{airlineSuffix}}'
-    )
-
-    airlines = (
-        'ABX Air', 'Air Choice One', 'Air Flamenco', 'Air Transport International', 'Air Wisconsin', 'AirNet Express', 'AirNow', 'AirTran Airways', 'Alaska Airlines', 'Allegiant Air', 'Aloha Air Cargo', 'American Airlines', 'American Eagle Airlines', 'Ameriflight', 'Amerijet International', 'Ameristar Air Cargo', 'Arctic Circle Air', 'Asia Pacific Airlines', 'Astar Air Cargo', 'Atlas Air',
-        'Bering Air',
-        'Cape Air', 'Capital Cargo International Airlines', 'Centurion Air Cargo', 'Chautauqua Airlines', 'CommutAir', 'Compass Airlines',
-        'Delta Air Lines', 'Delta Private Jets',
-        'Empire Airlines', 'Empire Airways', 'Era Alaska', 'Evergreen International Airlines', 'Everts Air', 'Executive Airlines', 'Express.Net Airlines', 'ExpressJet Airlines',
-        'FedEx Express', 'Flight Alaska', 'Freight Runners Express', 'Frontier Airlines', 'Frontier Flying Service',
-        'GeorgiaSkies', 'go!', 'GoJet Airlines', 'Grant Aviation', 'Great Lakes Airlines', 'Gryphon Airlines',
-        'Hageland Aviation Services', 'Hawaiian Airlines', 'Horizon Air',
-        'Island Air',
-        'JetBlue Airways',
-        'Kalitta Air', 'Kenmore Air',
-        'L-3 Flight International Aviation',
-        'Mesa Airlines', 'Miami Air International', 'Mokulele Airlines',
-        'Nantucket Airlines', 'National Airlines', 'NetJets', 'North American Airlines', 'Northern Air Cargo',
-        'Omni Air International',
-        'Pacific Wings', 'PenAir', 'Piedmont Airlines', 'Pinnacle Airlines', 'Polar Air Cargo', 'PSA Airlines',
-        'Republic Airlines', 'Ryan Air Services',
-        'San Juan Airlines', 'SeaCoast Airlines', 'SeaPort Airlines', 'Servant Air', 'Shuttle America', 'Silver Airways', 'Sky King', 'SkyWest Airlines', 'Southern Air', 'Southwest Airlines', 'Spirit Airlines', 'Sun Air International', 'Sun Country Airlines', 'Swift Air',
-        'Taquan Air', 'Tepper Aviation', 'Tradewinds Airlines', 'Trans Executive Airlines', 'Trans States Airlines',
-        'Ultimate Air Shuttle', 'United Airlines', 'UPS Airlines', 'US Airways', 'USA Jet Airlines',
-        'Vieques Air Link', 'Virgin America', 'Vision Airlines',
-        'Warbelow''s Air Ventures', 'Wiggins Airways', 'Wings of Alaska', 'World Airways', 'Wright Air Service',
-        'Xtra Airways',
-    )
-
-    airlineSuffixes = (
-        'Air', 'Aviation', 'Airlines', 'Airways', 'Aviation Service',
-        'Business Jets',
-        'Cargo', 'Charter', 'Commuter Jet',
-        'Express', 'Eagle',
-        'International',
-        'Jets',
-        'Transport',
-        'Wings'
-    )
-
-    @classmethod
-    def airlineSuffix(cls):
-        return cls.randomElement(cls.airlineSuffixes)
+    def __init__(self, generator):
+        """ Load the airplines list from airlines.dat """
+        self.__class__.airlines[:] = []    # clear the list
+        fieldnames = ["id", "name", "alias", "iata", "icao", "callsign", "country", "active"]
+        dir = os.path.dirname(__file__)
+        with open(os.path.join(dir, 'airlines.dat')) as airlineData:
+            self.__class__.airlines = [row for row in csv.DictReader(airlineData, delimiter=",", fieldnames=fieldnames)]
